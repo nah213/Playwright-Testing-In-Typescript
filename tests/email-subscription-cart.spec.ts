bce9c4f497349e41e8519125f6f8c3fd26ef8629
nah213 <nah213@hotmail.com>
2025-02-12 19:09:11 -0800

    Adding Email Subscription from Cart page Tests

---
+++ tests/email-subscription-cart.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Email Subscription From Cart Page Tests', () => {
+    test('Browsing to https://automationexercise.com loads the home page.', async ({ page }) => {
+        await page.goto('https://automationexercise.com');
+
+        await expect(page).toHaveTitle('Automation Exercise');
+    });
+
+    test('Verify Subscription input is aviable on the cart page.', async ({ page }) => {
+        await page.goto('https://automationexercise.com');
+
+        await page.getByRole('link', { name: ' Cart' }).click();
+
+        await expect(page.locator('#footer').locator('h2')).toHaveText('Subscription');
+        await expect(page.locator('#susbscribe_email')).toBeVisible();
+    });
+
+    test('After filling email then selecting submit the user presented with success message.', async ({ page }) => {
+        await page.goto('https://automationexercise.com');
+        await page.getByRole('link', { name: ' Cart' }).click();
+        await page.locator('#susbscribe_email').fill('test@test.com');
+        await page.locator('#subscribe').click();
+
+        await expect(page.locator('#success-subscribe')).toBeVisible();
+    });
+});
